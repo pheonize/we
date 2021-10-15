6e7d4a81b508d3539db9c7540129146324b88ee1
(CNX) runganunt <runganunt.tt@gmail.com>
2021-10-15 12:41:16 +0700

    init

---
+++ lib/loading_page.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class loading_page extends StatefulWidget {
+  const loading_page({ Key? key }) : super(key: key);
+
+  @override
+  Loading_pageState createState() => Loading_pageState();
+}
+
+class Loading_pageState extends State<loading_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
